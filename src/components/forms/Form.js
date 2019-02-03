997718405b4325d901a3b5942745774a68a0b385
Marcus <mojave27@gmail.com>
2019-02-03 10:14:50 -0700

    install reach-router

---
+++ src/components/forms/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    render(){
+        return(
+            <div>
+                form goes here
+            </div>
+        )
+    }
+}
